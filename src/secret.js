596ec06656a89070fdecdbc082404d722cc3b4d6
Sakib0017 <sakibkamal0017@gmail.com>
2024-09-03 12:32:28 +0600

    vercel.json updated

--- src/secret.js
+++ src/secret.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-const serverPort = process.env.SERVER_PORT || 3000;
-const MongodbURL = process.env.MONGODB_ATLAS_URL;
+const serverPort = 3007;
+const MongodbURL = "mongodb+srv://Admin:devpdcl@webdb.p9ofp.mongodb.net/test?retryWrites=true&w=majority&appName=webDB";
 const defaultImagePath = process.env.DEFAULT_USER_IMAGE_PATH || '/public/images/users/avatar.png';
 
 module.exports = {serverPort, MongodbURL, defaultImagePath}
